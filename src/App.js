4aa940dabaec85f28f831c0fc66eeef213736419
Nasif Wasek Fahim <nasiffahim51@gmail.com>
2023-06-09 19:02:06 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import './App.css';
+
+import Form from './components/Form';
+import TodoList from './components/TodoList';
+
+function App() {
+
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  return (
+    <div className="App">
+      <header>
+        <h1>My todo list</h1>
+      </header>
+      <Form inputText={inputText} 
+            setInputText={setInputText} 
+            todos={todos} 
+            setTodos={setTodos}
+      />
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
